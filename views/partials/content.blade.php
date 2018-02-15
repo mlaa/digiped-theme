d61ff43ecd0d22b3f718b44d8cf01a5abf938860
Ryan Williams <rwilliams@mla.org>
2018-02-15 17:02:09 -0500

    replace {{ with @php

--- views/partials/content.blade.php
+++ views/partials/content.blade.php
@@ -6,7 +6,7 @@
       <img src="//via.placeholder.com/200x150">
     @endif
     <header class="pv2 ph3">
-      <h2 class="entry-title ma0 mb1 f6"><a href="{{ get_permalink() }}" class="link">{{ get_the_title() }}</a></h2>
+      <h2 class="entry-title ma0 mb1 f6"><a href="@php(the_permalink())" class="link">@php(the_title())</a></h2>
       <i class="fas fa-tag mr1 f7"></i>
       <ul class="list di ttu ma0 pa0 f7">
         @php(the_tags('<li class="dib">', '</li><li class="dib">', '</li>'))
