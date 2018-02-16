a9b08a86530a4f56031a2085758d05ed1e321ead
Ryan Williams <rwilliams@mla.org>
2018-02-16 17:01:47 -0500

    simplify js in es6 class

--- resources/assets/scripts/digiped.js
+++ resources/assets/scripts/digiped.js
@@ -7,46 +7,38 @@ export default class DigiPed {
     // temporary debug helper
     $( 'aside a' ).on('click', this.redraw.bind(this));
 
-    // sidebar collections
-    //$('.my-digiped div > div').each(this.init_collection);
-
     // main grid
-    var m = new Muuri('main', {
+    this.addGrid('main');
+
+    // sidebar collections
+    // 'this' scope is different, use 'inst'
+    var inst = this;
+    $('.my-digiped div > div').map((i, elem) => {
+      inst.addGrid(elem);
+    });
+  }
+
+  addGrid(elem, options = {}) {
+    var inst = this;
+    var defaultOptions = {
       dragEnabled: true,
-      dragSort: this.grids.bind(this),
+      dragSort: () => {return inst.grids},
       dragSortInterval: 10,
-    })
-    .on('dragReleaseEnd', this.redraw.bind(this));
+    };
+
+    Object.assign(options, defaultOptions);
+
+    var m = new Muuri(elem, options);
+
+    m.on('dragReleaseEnd', this.redraw.bind(this));
 
     this.grids.push(m);
-  }
 
-  get grids() {
-    return this.constructor._grids;
+    return m;
   }
-
-  set grids(value) {
-    this.constructor._grids = value;
-    this.redraw.bind(this.constructor);
-  }
-
-  //  // TODO _grids grids
-//  init_collection(el) {
-//    console.log(el);
-//    var muuri = new Muuri(el, {
-//      items: 'article',
-//      dragEnabled: true,
-//      dragSort: this.grids,
-//      dragSortInterval: 10,
-//    });
-//
-//    muuri.on('dragReleaseEnd', this.redraw);
-//
-//    this._grids.push(muuri);
-//  }
 
   redraw() {
-    this.grids.forEach(function(grid) {
+    this.grids.map((grid) => {
       grid.refreshItems(); // only necessary when changing item dimensions, not sorting
       grid.layout();
     });
