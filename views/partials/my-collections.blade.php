172741a4c903521ff87c0c9b5d0410f01da8f029
Ryan Williams <rwilliams@mla.org>
2018-02-21 17:28:40 -0500

    fix grow/shrink for new collection setup

--- views/partials/my-collections.blade.php
+++ views/partials/my-collections.blade.php
@@ -1,8 +1,11 @@
 <div class="my-digiped">
-  <br><br><a class="link" href="#" onclick="javascript:jQuery('article').addClass('open');return false;">grow all cards</a>
-  <br><br><a class="link" href="#" onclick="javascript:jQuery('article').removeClass('open');return false;">shrink all cards</a>
+  <a class="link" href="#" onclick="javascript:jQuery('article').addClass('open');return false;">grow all cards</a><br>
+  <a class="link" href="#" onclick="javascript:jQuery('article').removeClass('open');return false;">shrink all cards</a><br>
 
-  <p>Drag &amp; drop cards on a collection.</p>
   <div class="collections"></div>
-  <br><br><i class="fa fa-plus-circle mr1"></i><a href="#" class="create-collection">Create new collection</a>
+  <div class="mv2">
+    <a href="#" class="link db pa2 bg-black white dim pointer create-collection">
+      <i class="fa fa-plus-circle mr1"></i> Create new collection
+    </a>
+  </div>
 </div>
