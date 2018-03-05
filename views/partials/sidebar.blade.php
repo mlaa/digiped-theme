079a63a9cb0f795389e9132d22911e9643a5f843
Ryan Williams <rwilliams@mla.org>
2018-03-05 09:08:30 -0500

    add placeholder sidebar content

--- views/partials/sidebar.blade.php
+++ views/partials/sidebar.blade.php
@@ -1,11 +1,16 @@
-<div class="pa3">
-  @if(is_category())
-  @endif
+<div>
+	<div class="pa2">
+		<p>Sit ratione quos nostrum aut aut tempore. Adipisci rerum fugit vel voluptatem quidem quasi. Harum fugiat fuga ut ut ut adipisci. Quidem id minus inventore eius et quia. Nostrum qui ex suscipit repudiandae. Architecto et et consequatur quod dolores libero.</p>
+		<p>Quia consequatur sed sed quam provident. Et dolor quos maxime voluptas maxime dolores velit. Rerum illum impedit corporis alias voluptas autem. Laborum non eum porro sint autem placeat. Et ipsam aut esse maxime.</p>
+		<p>At laboriosam omnis sapiente velit. Labore enim doloribus commodi similique voluptas dolor veritatis. Perferendis exercitationem hic sed ratione molestias.</p>
+		<p>Vitae harum enim quis laboriosam. Consequatur tempora molestias quidem in. Nemo ut ut qui in fugiat quisquam est. Id quas qui culpa sunt aut.</p>
+		<p>Et aut consectetur eveniet maiores omnis et. Omnis iste ducimus non aut fugit minus quod similique. Consectetur ducimus dolore fugiat vero debitis.</p>
+		@if(is_category())
+		@endif
+		{{--
+		@php(dynamic_sidebar('sidebar-primary'))
+		--}}
+	</div>
 
-  {{--
-  @php(dynamic_sidebar('sidebar-primary'))
-  --}}
-
-  {{-- my digiped --}}
-  @include('partials/my-collections')
+	@include('partials/my-collections')
 </div>
