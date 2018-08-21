d521851badfc5121dd65baa98db4e5262b67146f
Ryan Williams <rwilliams@mla.org>
2018-08-21 16:23:35 -0400

    add initial header content/style

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -1,10 +1,14 @@
 <header class="banner">
-  <div class="container pa3 bb">
-    <a class="brand f4" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+	<div class="container cf pa2">
+		<div class="fl w-50 tl">
+			<a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+		</div>
+		<div class="fl w-50 tr">
+			<nav class="nav-primary">
+				@if (has_nav_menu('primary_navigation'))
+					{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+				@endif
+			</nav>
+		</div>
+	</div>
 </header>
