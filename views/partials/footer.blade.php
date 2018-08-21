bdc83368a0f9178298e48d612244e5c086b14bf1
Ryan Williams <rwilliams@mla.org>
2018-08-21 15:42:07 -0400

    add initial footer content/style

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,5 +1,11 @@
 <footer class="content-info">
-  <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
-  </div>
+  <div class="container cf pv1 ph2">
+		<div class="fl w-50 tl">
+			@php(dynamic_sidebar('sidebar-footer'))
+		</div>
+		<div class="fl w-50 tr">
+			<p>Digital Pedagogy in the Humanities</p>
+			<img src="/app/themes/digiped-theme/dist/images/mla-logo.png" alt="MLA" \>
+		</div>
+	</div>
 </footer>
--- views/partials/my-collections.blade.php
+++ views/partials/my-collections.blade.php
@@ -1,4 +1,4 @@
-<div class="my-digiped fixed bottom-0 overflow-y-scroll mw5 bg-white">
+<div class="my-digiped bottom-0 overflow-y-scroll mw5 bg-white">
 	<div class="collections min-h-100"></div>
 	<a href="#" class="link db pa3 bg-black white dim pointer create-collection">
 		<i class="fa fa-plus-circle mr1"></i> Create new collection
