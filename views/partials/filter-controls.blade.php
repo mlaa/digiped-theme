12f13fea06a826f6c147efaace3b4437f89c5014
Eric Knappe <eknappe@mla.org>
2018-12-28 13:59:16 -0500

    editing the menubar in the filte-control blad for effect

--- views/partials/filter-controls.blade.php
+++ views/partials/filter-controls.blade.php
@@ -1,13 +1,13 @@
 FILTER ARTIFACTS<ul class="nav list dib ttu mh3 pa0">
   <li class="bl pl3 di b"><a href="#" class="tag black link">TAG</a></li>
   <li class="bl pl3 ml3 di"><a href="#" class="type black link">TYPE</a></li>
-  <li class="bl pl3 ml3 di"><a href="#" class="keyword black link">KEYWORD</a></li>
   <li class="bl pl3 ml3 di"><a href="#" class="author black link">AUTHOR</a></li>
+  <li class="bl pl3 ml3 di"><a href="#" class="curator black link">CURATOR</a></li>
 </ul>
 
 <div class="options">
   <ul class="tag list di ttu ma0 pa0"></ul>
   <ul class="type list dn ttu ma0 pa0"></ul>
-  <ul class="keyword list dn ttu ma0 pa0"></ul>
   <ul class="author list dn ttu ma0 pa0"></ul>
+  <ul class="curator list dn ttu ma0 pa0"></ul>
 </div>
