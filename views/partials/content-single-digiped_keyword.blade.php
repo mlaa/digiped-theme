8c83f8e7b6882468cd8c62055327a472439c1730
Eric Knappe <eknappe@mla.org>
2019-01-02 11:13:18 -0500

    added themeplate fonts

---
+++ views/partials/content-single-digiped_keyword.blade.php
@@ -0,0 +1,27 @@
+<div 
+@php(post_class('keyword'))>
+  <div class="keyword-wrapper">
+    <header>
+      <h2 class="sub-title"><div class="keyword">Keyword</div></h1>
+      <h1 class="title">{{ get_the_title() }}</h1>
+      @include('partials/entry-meta-keyword')
+      <h2 class="sub-title"><div class="statement">Curatorial Statement</div></h2>
+    </header>
+    <div>
+      <?php the_content(); ?>
+      <div id="read-more">Read Less</div>
+    </div>  
+    <header>
+      <h2 class="sub-title"><div class="artifacts">Artifacts</div></h2>
+    </header>
+    <div class="grid relative">
+      <div class="entry-content"> 
+        <?php $the_artifacts = new WP_Query( 'post_type=digiped_artifact', 'tag='.get_the_title() ); ?>
+        @foreach($the_artifacts as $artifact)
+        @include('partials.content-digiped-keyword-panel', $artifact)
+        @endforeach
+      </div>
+    </div>
+  </div>
+  @php(comments_template('/partials/comments.blade.php'))
+</div>
---
+++ views/partials/entry-meta-keyword.blade.php
@@ -0,0 +1,11 @@
+<?php 
+    $metadata = get_post_meta(get_the_ID()); 
+    $creators = $metadata['creator(s)'][0];
+    $license = $metadata['license'][0];
+    $type = $metadata['type'][0];
+    $url = $metadata['url'][0];
+if ($creators) { ?>
+  <h2 class='sub-title'><div class='curator'>Curator</div></h1>
+      <h3 class='curator-name'><?php echo $creators; ?></h3>
+<?php } ?>
+
